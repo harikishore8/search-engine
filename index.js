0bcf66748b9f11bdf56217b8281273acce1d959b
Hari Kishore <88875466+harikishore8@users.noreply.github.com>
2022-05-16 09:48:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 let main = document.getElementById('main');
 
 
-
 async function FindMe(e)
 {
     e.preventDefault();
-    let name = document.getElementById('inp').value;
-    let res= await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyA5U7lnFOgfm2xP7j7H5HSZvyH9oF4kMjE&cx=8d59952f3963391e6&q='+name);
+    let name1 = document.getElementById('inp').value;
+    let res= await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyA5U7lnFOgfm2xP7j7H5HSZvyH9oF4kMjE&cx=8d59952f3963391e6&q='+name1);
     let result = await res.json();
     let data = result.items;
     
@@ -22,4 +21,38 @@ async function FindMe(e)
         main.appendChild(div);
 
     })
+
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(name1,time);
+    let arr1 = [];
+    arr1.push(name1,time);
+    console.log(arr1);
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+    
