50ebfa6d87f3f02dad4493ee278193c926e9136f
Hari Kishore <88875466+harikishore8@users.noreply.github.com>
2022-07-20 11:20:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,29 +30,10 @@ async function FindMe(e)
     console.log(arr1);
 }
 
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const element = document.getElementById("ez");
+element.addEventListener("click", myFunction);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function myFunction() {
+  main.innerHTML="";
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-    
